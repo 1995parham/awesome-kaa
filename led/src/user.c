d90c972f286fbfe5009da45daf2d31874f83dead
Parham Alvani <parham.alvani@gmail.com>
2016-02-28 23:02:38 +0330

    Improve user attaching process .. but nothing ..

--- led/src/user.c
+++ led/src/user.c
@@ -23,25 +23,31 @@
 
 static kaa_error_t on_attached(void *context, const char *user_external_id, const char *endpoint_access_token)
 {
-	    return KAA_ERR_NONE;
+	printf("Attached :)\n");
+	return KAA_ERR_NONE;
 }
 
 static kaa_error_t on_detached(void *context, const char *endpoint_access_token)
 {
-	    return KAA_ERR_NONE;
+	printf("Detached :|\n");
+	return KAA_ERR_NONE;
 }
 static kaa_error_t on_attach_success(void *context)
 {
-	    return KAA_ERR_NONE;
+	printf("Success Attach :D\n");
+	return KAA_ERR_NONE;
 }
 static kaa_error_t on_attach_failed(void *context, user_verifier_error_code_t error_code, const char *reason)
 {
-	    return KAA_ERR_NONE;
+	printf("Attach Failed :(\n");
+	return KAA_ERR_NONE;
 }
 
 kaa_error_t attach_endpoint_to_user(kaa_user_manager_t *manager, const char *user_external_id, const char *user_access_token)
 {
 	kaa_error_t error_code;
+
+	printf("Start attaching to the user ...\n");
 
 	kaa_attachment_status_listeners_t attachement_listeners = {
 		NULL,
@@ -53,10 +59,12 @@ kaa_error_t attach_endpoint_to_user(kaa_user_manager_t *manager, const char *use
 	
 	error_code = kaa_user_manager_set_attachment_listeners(manager,
 			&attachement_listeners);
+	KAA_RETURN_IF_ERROR(error_code, "Failed to user attaching listeners");
 	
 	error_code = kaa_user_manager_default_attach_to_user(manager,
 			user_external_id,
 			user_access_token);
+	KAA_RETURN_IF_ERROR(error_code, "Failed to attach to the user");
 
 	return error_code;
 }
