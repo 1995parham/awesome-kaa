a1171a5e0ad2cb91728a7b4c3e3b1740864d4ce8
Parham Alvani <parham.alvani@gmail.com>
2017-05-30 14:52:30 +0430

    Improves main file :joy:

--- examples/license-plate/main.c
+++ examples/license-plate/main.c
@@ -16,21 +16,13 @@
 # include "collections/kaa_list.h"
 
 
-//static int32_t sample_period;
-//static time_t  last_sample_time;
 extern kaa_error_t ext_unlimited_log_storage_create(void **log_storage_context_p, kaa_logger_t *logger);
-/* Retrieves current 
-perature. */
-//static int32_t get_temperature_sample(void)
-//{
-    /* For the sake of example, random data is used */
-//    return rand() % 10 + 25;
-//}
-/* Periodically called by Kaa SDK. */
+
 char* gplate;
+
+/* Periodically called by Kaa SDK. */
 static void example_callback(void *context)
-{  
-       
+{
 	kaa_string_t plate;
 	plate.data = gplate ;
         kaa_user_log_record_t *log_record = kaa_logging_data_collection_create();
@@ -42,8 +34,7 @@ static void example_callback(void *context)
 int main(int argc, char *argv[])
 {
     gplate = argv[1];
-    /* Init random generator used to generate temperature */
-    srand(time(NULL));
+
     /* Prepare Kaa client. */
     kaa_client_t *kaa_client = NULL;
     kaa_error_t error = kaa_client_create(&kaa_client, NULL);
@@ -53,12 +44,14 @@ int main(int argc, char *argv[])
 
     void *log_storage_context         = NULL;
     void *log_upload_strategy_context = NULL;
+
     /* The internal memory log storage distributed with Kaa SDK. */
     error = ext_unlimited_log_storage_create(&log_storage_context,
         kaa_client_get_context(kaa_client)->logger);
     if (error) {
         return EXIT_FAILURE;
     }
+
     /* Create a strategy based on timeout. */
     error = ext_log_upload_strategy_create(
         kaa_client_get_context(kaa_client), &log_upload_strategy_context,
@@ -66,28 +59,31 @@ int main(int argc, char *argv[])
     if (error) {
         return EXIT_FAILURE;
     }
+
     /* Strategy will upload logs every 5 seconds. */
     error = ext_log_upload_strategy_set_upload_timeout(log_upload_strategy_context, 5);
     if (error) {
         return EXIT_FAILURE;
     }
+
     /* Specify log bucket size constraints. */
     kaa_log_bucket_constraints_t bucket_sizes = {
          .max_bucket_size       = 32,   /* Bucket size in bytes. */
          .max_bucket_log_count  = 2,    /* Maximum log count in one bucket. */
     };
+
     /* Initialize the log storage and strategy (by default, they are not set). */
     error = kaa_logging_init(kaa_client_get_context(kaa_client)->log_collector,
         log_storage_context, log_upload_strategy_context, &bucket_sizes);
     if (error) {
         return EXIT_FAILURE;
     }
-    
+
     /* Start Kaa SDK's main loop. example_callback is called once per second. */
     error = kaa_client_start(kaa_client, example_callback, kaa_client, 1);
     /* Should get here only after Kaa stops. */
     kaa_client_destroy(kaa_client);
-    
+
     if (error) {
         return EXIT_FAILURE;
     }
