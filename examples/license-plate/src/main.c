eda0abec967772c01990e9f366d6805a837298bd
Parham Alvani <parham.alvani@gmail.com>
2017-07-01 22:45:34 +0430

    Moves and improves :)

--- examples/license-plate/src/main.c
+++ examples/license-plate/src/main.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <time.h>
 #include <kaa.h>
 #include <platform/kaa_client.h>
-#include <kaa_error.h>
 #include <kaa_configuration_manager.h>
 #include <kaa_logging.h>
-#include <gen/kaa_logging_gen.h>
-#include <platform/kaa_client.h>
 #include <utilities/kaa_log.h>
-#include <platform-impl/common/ext_log_upload_strategies.h>
+#include <kaa/platform-impl/common/ext_log_upload_strategies.h>
 
-# include "kaa_common_schema.h"
-# include "collections/kaa_list.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <kaa/gen/kaa_plate_detection_event_definitions.h>
+#include <kaa/gen/kaa_plate_detection_event.h>
+#include <extensions/user/kaa_user.h>
 
 
 extern kaa_error_t ext_unlimited_log_storage_create(void **log_storage_context_p, kaa_logger_t *logger);
 
-char* gplate;
-
 /* Periodically called by Kaa SDK. */
 static void example_callback(void *context)
 {
-	kaa_string_t plate;
-	plate.data = gplate ;
+
+        kaa_string_t plate;
         kaa_user_log_record_t *log_record = kaa_logging_data_collection_create();
+        plate.data = "parham";
         log_record->plate = &plate;
         kaa_logging_add_record(kaa_client_get_context(context)->log_collector, log_record, NULL);
- 	printf("Sampled temperature: %s\n", plate);
+        printf("%s uploaded\n", plate.data);
+
+        kaa_plate_detection_event_plate_event_t *plate_event = kaa_plate_detection_event_plate_event_create();
+        plate_event->plate = &plate;
+        kaa_error_t error_code = kaa_event_manager_send_kaa_plate_detection_event_plate_event(
+                kaa_client_get_context(context)->event_manager, plate_event, NULL);
+        //plate_event->destroy(plate_event);
+        printf("%s event sent\n", plate.data);
+}
+
+void *kaa_thread(void *context)
+{
+    printf("Hello kaa ...");
+    kaa_client_t *kaa_client = (kaa_client_t *) context;
+    /* Start Kaa SDK's main loop. example_callback is called once per second. */
+    kaa_client_start(kaa_client, NULL, NULL, 1);
+    kaa_user_manager_attach_to_user((kaa_user_manager_t *) kaa_client, "user", "", "");
+    /* Should get here only after Kaa stops. */
+    kaa_client_destroy(kaa_client);
 }
 
 int main(int argc, char *argv[])
 {
-    gplate = argv[1];
 
     /* Prepare Kaa client. */
     kaa_client_t *kaa_client = NULL;
@@ -44,14 +59,12 @@ int main(int argc, char *argv[])
 
     void *log_storage_context         = NULL;
     void *log_upload_strategy_context = NULL;
-
     /* The internal memory log storage distributed with Kaa SDK. */
     error = ext_unlimited_log_storage_create(&log_storage_context,
         kaa_client_get_context(kaa_client)->logger);
     if (error) {
         return EXIT_FAILURE;
     }
-
     /* Create a strategy based on timeout. */
     error = ext_log_upload_strategy_create(
         kaa_client_get_context(kaa_client), &log_upload_strategy_context,
@@ -59,33 +72,28 @@ int main(int argc, char *argv[])
     if (error) {
         return EXIT_FAILURE;
     }
-
     /* Strategy will upload logs every 5 seconds. */
     error = ext_log_upload_strategy_set_upload_timeout(log_upload_strategy_context, 5);
     if (error) {
         return EXIT_FAILURE;
     }
-
     /* Specify log bucket size constraints. */
     kaa_log_bucket_constraints_t bucket_sizes = {
          .max_bucket_size       = 32,   /* Bucket size in bytes. */
          .max_bucket_log_count  = 2,    /* Maximum log count in one bucket. */
     };
-
     /* Initialize the log storage and strategy (by default, they are not set). */
     error = kaa_logging_init(kaa_client_get_context(kaa_client)->log_collector,
         log_storage_context, log_upload_strategy_context, &bucket_sizes);
     if (error) {
         return EXIT_FAILURE;
     }
+    
+    pthread_t thread;
+    pthread_create(&thread, NULL, kaa_thread, kaa_client);
+    sleep(1);
+    example_callback(kaa_client);
 
-    /* Start Kaa SDK's main loop. example_callback is called once per second. */
-    error = kaa_client_start(kaa_client, example_callback, kaa_client, 1);
-    /* Should get here only after Kaa stops. */
-    kaa_client_destroy(kaa_client);
 
-    if (error) {
-        return EXIT_FAILURE;
-    }
     return EXIT_SUCCESS;
 }
