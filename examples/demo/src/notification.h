ae50da7bcdbac889705fbef27b8e9158078502ea
Parham Alvani <parham.alvani@gmail.com>
2017-07-24 23:36:38 +0430

    Notification header file was added.

---
+++ examples/demo/src/notification.h
@@ -0,0 +1,22 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : notification.h
+ *
+ * [] Creation Date : 24-07-2017
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2017 Parham Alvani.
+*/
+
+#ifndef NOTIFICATION_H
+#define NOTIFICATION_H
+
+#include <kaa/kaa_error.h>
+
+kaa_error_t kaa_notification_register(kaa_notification_manager_t *manager);
+
+#endif
