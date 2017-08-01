f65e22376ecef0e0eb028e0dea6e59a2196481a8
Parham Alvani <parham.alvani@gmail.com>
2017-08-01 09:44:26 +0430

    Lamp project was added.

---
+++ examples/lamp/src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <kaa.h>
+#include <platform/kaa_client.h>
+#include <extensions/notification/kaa_notification_manager.h>
+#include <kaa/platform/ext_notification_receiver.h>
+#include <unistd.h>
+
+static void on_notification(void *context, uint64_t *topic_id, kaa_notification_t *notification)
+{
+	printf("Received notification on topic %llu: message=%d\n", *topic_id, notification->turn);
+}
+
+int main(int argc, char *argv[])
+{
+	/* Prepare Kaa client. */
+	kaa_client_t *kaa_client = NULL;
+	kaa_error_t error = kaa_client_create(&kaa_client, NULL);
+	if (error) {
+		return EXIT_FAILURE;
+	}
+	printf("Hello kaa ...");
+	
+	kaa_notification_listener_t notification_listener = { on_notification, NULL };
+	uint32_t notification_listener_id = 0;
+
+	// Add listener
+	kaa_add_notification_listener(kaa_client_get_context(kaa_client)->notification_manager, &notification_listener, &notification_listener_id);
+	printf("%d\n", notification_listener_id);
+
+
+	/* Start Kaa SDK's main loop. example_callback is called once per second. */
+	kaa_client_start(kaa_client, NULL, NULL, 1);
+	/* Should get here only after Kaa stops. */
+	kaa_client_destroy(kaa_client);
+}
